ce9d0a374206366222b3565e1a6e4e0039969c49
Monroe <monroemallum@gmail.com>
2017-02-27 12:06:28 -0600

    GraphSettingsFragment: Added UI

--- Android_code/STEM-METER/app/src/main/java/com/stemmeter/stem_meter/GraphSettingsFragment.java
+++ Android_code/STEM-METER/app/src/main/java/com/stemmeter/stem_meter/GraphSettingsFragment.java
@@ -1,11 +1,13 @@
 package com.stemmeter.stem_meter;
 
 import android.app.Activity;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +29,7 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.stemmeter.stem_meter.Sensors.Sensor;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,11 +44,19 @@ public class GraphSettingsFragment extends Fragment {
     GraphSettingsFragInterface graphSettingsFragInterface;
     private String TAG = "GraphSettingsFragTag";
     private Spinner selectedSensorSpinner;
+    private ArrayList<String> dataPointNames;
+    private ArrayList<String> unitNames;
+    private DataListAdapter dataListAdapter;
+    private UnitListAdapter unitListAdapter;
+    private ImageButton doneBtn;
+    private final String GRAPH_FRAG_TAG = "GraphFragTag";
     // Container Activity must implement this interface
 
     // Container Activity must implement this interface
     public interface GraphSettingsFragInterface {
         public Sensor getSensor(int sensorNumber);
+        //public ArrayList<Boolean> getConnectedSensors();
+        public GraphConfig getGraphConfig();
     }
 
     GraphSettingsFragInterface graphSettingsInterface;
@@ -62,20 +73,24 @@ public class GraphSettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.graph_settings_fragment, container, false);
+
         selectedSensorSpinner = (Spinner) view.findViewById(R.id.SelectedSensorSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.frequency_array, android.R.layout.simple_spinner_item);
+                R.array.sensor_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         selectedSensorSpinner.setAdapter(adapter);
-
+        selectedSensorSpinner.setSelection(graphSettingsFragInterface.getGraphConfig().getSelectedSensor());
 
         // Set the spinner based on its SensorConfig object
         //selectedSensorSpinner.setSelection(sensorFragInterface.getSensorConfig(position+1).getFreq());
         selectedSensorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view,
-                                       int freqSelected, long id) {
+                                       int sensorSelected, long id) {
+                graphSettingsFragInterface.getGraphConfig().setSelectedSensor(sensorSelected);
+                reinializeDataListView();
+                reinitializeUnitListView();
             }
 
             @Override
@@ -84,8 +99,67 @@ public class GraphSettingsFragment extends Fragment {
             }
         });
 
+        dataListAdapter = new DataListAdapter();
+        GraphSettings graphSettings = graphSettingsFragInterface.getSensor(graphSettingsFragInterface.getGraphConfig().getSelectedSensor() + 1).getGraphSettings();
+        dataPointNames = graphSettings.getDataPoints();
+        if (dataPointNames.size() > 0)
+        {
+            for (int i = 0; i < dataPointNames.size(); i++)
+                dataListAdapter.addItem(dataPointNames.get(i));
+        }
+
+        ListView dataNameListView = (ListView) view.findViewById(R.id.SelectedSensorDatalist);
+        dataNameListView.setAdapter(dataListAdapter);
+
+        unitListAdapter = new UnitListAdapter();
+        unitNames = graphSettings.getUnits();
+        if (unitNames.size() > 0)
+        {
+            for (int i = 0; i < unitNames.size(); i++)
+                unitListAdapter.addItem(unitNames.get(i));
+        }
+
+        ListView unitNameListView = (ListView) view.findViewById(R.id.Unitlist);
+        unitNameListView.setAdapter(unitListAdapter);
+
+        doneBtn = (ImageButton) view.findViewById(R.id.DoneBtn);
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                GraphFragment graphFragment = new GraphFragment();
+                transaction.replace(R.id.fragment_container, graphFragment, GRAPH_FRAG_TAG);
+                //transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
 
         return view;
+    }
+
+    private void reinializeDataListView()
+    {
+        dataListAdapter.removeAllItems();
+        GraphSettings graphSettings = graphSettingsFragInterface.getSensor(graphSettingsFragInterface.getGraphConfig().getSelectedSensor() + 1).getGraphSettings();
+        dataPointNames = graphSettings.getDataPoints();
+        if (dataPointNames.size() > 0)
+        {
+            for (int i = 0; i < dataPointNames.size(); i++)
+                dataListAdapter.addItem(dataPointNames.get(i));
+        }
+    }
+
+    private void reinitializeUnitListView()
+    {
+        unitListAdapter.removeAllItems();
+        GraphSettings graphSettings = graphSettingsFragInterface.getSensor(graphSettingsFragInterface.getGraphConfig().getSelectedSensor() + 1).getGraphSettings();
+        unitNames = graphSettings.getUnits();
+        if (unitNames.size() > 0)
+        {
+            for (int i = 0; i < unitNames.size(); i++)
+                unitListAdapter.addItem(unitNames.get(i));
+        }
     }
 
     @Override
@@ -96,6 +170,148 @@ public class GraphSettingsFragment extends Fragment {
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement GraphSettingsFragInterface");
+        }
+    }
+
+    private class DataListAdapter extends BaseAdapter {
+        private ArrayList<String> dataPointName = new ArrayList<String>();
+        private LayoutInflater mInflater;
+        private String TAG = "CustomAdapter";
+        private int currentSelectedPosition;
+        private int selectedPosition = -1;
+
+        public DataListAdapter() {
+
+            mInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public void addItem(final String item) {
+            dataPointName.add(item);
+            notifyDataSetChanged();
+        }
+
+        public void updateItem(final String item, int position) {
+            dataPointName.set(position, item);
+
+        }
+
+        public void removeAllItems()
+        {
+            dataPointName.clear();
+        }
+
+        @Override
+        public int getCount() {
+            return dataPointName.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return dataPointName.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            final TextView dataNameText;
+            final ImageButton deleteBtn;
+
+            if (convertView == null) {
+                // if the view is null then inflate the custom item layout
+                convertView = mInflater.inflate(R.layout.data_name_list_item, null);
+            }
+
+            dataNameText = (TextView) convertView.findViewById(R.id.dataNameItemTextView);
+
+            if (dataNameText != null)
+                dataNameText.setText(dataPointName.get(position));
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+                    selectedPosition = position;
+                    notifyDataSetChanged();
+                }
+            });
+
+            return convertView;
+
+        }
+    }
+
+    private class UnitListAdapter extends BaseAdapter {
+        private ArrayList<String> unitName = new ArrayList<String>();
+        private LayoutInflater mInflater;
+        private String TAG = "CustomAdapter";
+        private int currentSelectedPosition;
+        private int selectedPosition = -1;
+
+        public UnitListAdapter() {
+
+            mInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public void addItem(final String item) {
+            unitName.add(item);
+            notifyDataSetChanged();
+        }
+
+        public void removeAllItems()
+        {
+            unitName.clear();
+        }
+
+        public void updateItem(final String item, int position) {
+            unitName.set(position, item);
+
+        }
+
+        @Override
+        public int getCount() {
+            return unitName.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return unitName.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            final TextView unitNameText;
+            final ImageButton deleteBtn;
+
+            if (convertView == null) {
+                // if the view is null then inflate the custom item layout
+                convertView = mInflater.inflate(R.layout.unit_name_list_item, null);
+            }
+
+            unitNameText = (TextView) convertView.findViewById(R.id.unitNameItemTextView);
+
+            if (unitNameText != null)
+                unitNameText.setText(unitName.get(position));
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+                    selectedPosition = position;
+                    notifyDataSetChanged();
+                }
+            });
+
+            return convertView;
+
         }
     }
 }
