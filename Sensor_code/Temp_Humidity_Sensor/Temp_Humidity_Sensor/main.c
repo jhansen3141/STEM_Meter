252aa9e007b94e077e9cc7629b09d32ff002875b
Josh <jhansne3141@gmail.com>
2017-03-04 14:57:59 -0600

    Reenabled power saving on CC2640

--- Sensor_code/Temp_Humidity_Sensor/Temp_Humidity_Sensor/main.c
+++ Sensor_code/Temp_Humidity_Sensor/Temp_Humidity_Sensor/main.c
@@ -90,57 +90,58 @@ void setSensorFreq(int arg_cnt, char **args) {
 	
 	switch (freqCommand) {
 		case RATE_OFF:
-		return;
-		break;
+			return;
+			break;
+		
 		case RATE_TEN_HZ:
-		OCR1A = TIMER_TEN_HZ_NUM;
-		sensorRate = RATE_TEN_HZ;
-		break;
+			OCR1A = TIMER_TEN_HZ_NUM;
+			sensorRate = RATE_TEN_HZ;
+			break;
 		
 		case RATE_FIVE_HZ:
-		OCR1A = TIMER_FIVE_HZ_NUM;
-		sensorRate = RATE_FIVE_HZ;
-		break;
+			OCR1A = TIMER_FIVE_HZ_NUM;
+			sensorRate = RATE_FIVE_HZ;
+			break;
 		
 		case RATE_ONE_SEC:
-		OCR1A = TIMER_ONE_HZ_NUM;
-		sensorRate = RATE_ONE_SEC;
-		break;
+			OCR1A = TIMER_ONE_HZ_NUM;
+			sensorRate = RATE_ONE_SEC;
+			break;
 		
 		case RATE_FIVE_SEC:
-		OCR1A = TIMER_ONE_HZ_NUM;
-		sensorRate = RATE_FIVE_SEC;
-		break;
+			OCR1A = TIMER_ONE_HZ_NUM;
+			sensorRate = RATE_FIVE_SEC;
+			break;
 		
 		case RATE_TEN_SEC:
-		OCR1A = TIMER_ONE_HZ_NUM;
-		sensorRate = RATE_TEN_SEC;
-		break;
+			OCR1A = TIMER_ONE_HZ_NUM;
+			sensorRate = RATE_TEN_SEC;
+			break;
 		
 		case RATE_THIRTY_SEC:
-		OCR1A = TIMER_ONE_HZ_NUM;
-		sensorRate = RATE_THIRTY_SEC;
-		break;
+			OCR1A = TIMER_ONE_HZ_NUM;
+			sensorRate = RATE_THIRTY_SEC;
+			break;
 		
 		case RATE_ONE_MIN:
-		OCR1A = TIMER_ONE_HZ_NUM;
-		sensorRate = RATE_ONE_MIN;
-		break;
+			OCR1A = TIMER_ONE_HZ_NUM;
+			sensorRate = RATE_ONE_MIN;
+			break;
 		
 		case RATE_TEN_MIN:
-		OCR1A = TIMER_ONE_HZ_NUM;
-		sensorRate = RATE_TEN_MIN;
-		break;
+			OCR1A = TIMER_ONE_HZ_NUM;
+			sensorRate = RATE_TEN_MIN;
+			break;
 		
 		case RATE_THIRTY_MIN:
-		OCR1A = TIMER_ONE_HZ_NUM;
-		sensorRate = RATE_THIRTY_MIN;
-		break;
+			OCR1A = TIMER_ONE_HZ_NUM;
+			sensorRate = RATE_THIRTY_MIN;
+			break;
 		
 		case RATE_ONE_HOUR:
-		OCR1A = TIMER_ONE_HZ_NUM;
-		sensorRate = RATE_ONE_HOUR;
-		break;
+			OCR1A = TIMER_ONE_HZ_NUM;
+			sensorRate = RATE_ONE_HOUR;
+			break;
 		
 	}
 	secondCounter = 0;
